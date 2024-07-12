4904ab01a2d6c5a7c2ae12a485229498a25775d1
thomasisa1 <thomasisa1@users.noreply.github.com>
2024-07-12 23:50:07 +0000

    update home.js

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,14 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 const Home = () => {
-    const { store, actions } = useContext(Context);
-
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Welcome to the Home Page</h1>
             <p>This is the home page of your application.</p>
-            {/* You can use store and actions here as needed */}
+            <Link to="/login" className="btn btn-primary">Login</Link>
+            <Link to="/signup" className="btn btn-secondary ms-3">Signup</Link>
         </div>
     );
 };
